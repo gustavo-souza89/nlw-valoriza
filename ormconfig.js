03888fd2942895cdbf8ec85db403a61f39b21c79
Gustavo Souza <gustavo.souzagu@gmail.com>
2021-06-26 05:26:40 -0300

    Heroku - 4

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,12 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": true, "extra": { "ssl": { "rejectUnauthorized": false }},
   "migrations": ["./dist/database/migration/*.js"],
   "entities": ["dist/entities/*.js"],
   "cli": {
     "migrationsDir": "./src/database/migration",
     "entitiesDir": "src/entities"
   }
+  
 }
